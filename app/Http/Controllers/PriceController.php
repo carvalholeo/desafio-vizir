6d74dcecf557c5cc4b462092d716efd6087674df
Léo Carvalho <carvalho.csleo@gmail.com>
2019-07-03 00:30:15 -0300

    Cria a lógica de negócio do Controlador

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -6,79 +6,74 @@ use Illuminate\Http\Request;
 
 class PriceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function calcular(Request $request)
     {
-        //
+        $dddOrigem = $request->input('dddOrigem');
+        $dddDestino = $request->input('dddDestino');
+        $minutosGastos = $request->input('minutosGastos');
+        $planoEscolhido = $request->input('planoEscolhido');
+
+        if($dddOrigem == $dddDestino) {
+            return response("DDD de origem não pode ser ao igual DDD de destino", 400);
+        }
+
+        if($minutosGastos < 0) {
+            return response("Quantidade de minutos precisa maior ou igual a zero", 400);
+        }
+
+        if(($dddOrigem == 16 && ($dddDestino == 17 || $dddDestino == 18)) ||
+        ($dddOrigem == 17 && ($dddDestino == 16 || $dddDestino == 18)) ||
+        ($dddOrigem == 18 && ($dddDestino == 16 || $dddDestino == 17))) {
+            return response("Ainda não estamos atendendo essa rota de ligação. Fique ligado, estamos ampliando nossas operações rapidamente!", 400);
+        }
+
+        switch($dddOrigem) {
+            case 11:
+                if($dddDestino == 16) {
+                    $custoOriginal = 1.90;
+                    $custoAdicional = $custoOriginal + ($custoOriginal * 0.10);
+                } elseif ($dddDestino == 17) {
+                    $custoOriginal = 1.70;
+                    $custoAdicional = $custoOriginal + ($custoOriginal * 0.10);
+                } else {
+                    $custoOriginal = 0.90;
+                    $custoAdicional = $custoOriginal + ($custoOriginal * 0.10);
+                }
+                break;
+            case 16:
+                $custoOriginal = 2.90;
+                $custoAdicional = $custoOriginal + ($custoOriginal * 0.10);
+                break;
+            case 17:
+                $custoOriginal = 2.70;
+                $custoAdicional = $custoOriginal + ($custoOriginal * 0.10);
+                break;
+            case 18:
+                $custoOriginal = 1.90;
+                $custoAdicional = $custoOriginal + ($custoOriginal * 0.10);
+                break;
+        }
+        
+        if ($minutosGastos > $planoEscolhido) {
+            $minutosRestantes = $minutosGastos - $planoEscolhido;
+            $precoComPlano = $minutosRestantes * $custoAdicional;
+            $precoSemPlano = $minutosGastos * $custoOriginal;
+        } else {
+            $precoComPlano = 0.00;
+            $precoSemPlano = $minutosGastos * $custoOriginal;
+        }
+
+        $plano = "{
+            \"dddOrigem\": $dddOrigem,
+            \"dddDestino\": $dddDestino,
+            \"minutosGastos\": $minutosGastos,
+            \"planoEscolhido\": $planoEscolhido,
+            \"precoComPlano\": $precoComPlano,
+            \"precoSemPlano\": $precoSemPlano
+        }";
+        
+        return $plano;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
