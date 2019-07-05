4a80a5247a0861f2b846d74d800df9c03191927c
Leonardo Carvalho <carvalho.csleo@gmail.com>
2019-07-05 20:48:31 -0300

    Atualiza o conteúdo da página "Sobre nós"

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -10,8 +10,8 @@ Telzir - Sobre a Telzir
         <p class="text-left">Somos uma operadora jovem, inovadora, como foco em dar às pessoas uma nova possibilidade de fazer ligações de longa distância de forma mais barata.</p>
         <p class="text-left">A Telzir fornece os melhores serviços para você se conectar com aquilo que mais importa para você. A ligação te faz ficar mais próximo e ligado para:</p>
         <ul>
-            <li>Falar com um parente que está em viagem pelo interior do Brasil</li>
-            <li>Fechar aquele contrato com o maior cliente da sua empresa</li>
+            <li>Falar com seu parente ou amigo que está viajando pelo Brasil</li>
+            <li>Fechar aquele contrato com um novo cliente da sua empresa</li>
             <li>Ligar para os seus programas de TV favoritos</li>
         </ul>
         <p class="text-left">Nossa missão é democratizar o acesso à comunicação. Acreditamos que falar com quem você ama e resolver seus assuntos por telefone não precisa custar caro. Você pode e DEVE ter um serviço de alta qualidade e que tenha um preço justo.</p>
