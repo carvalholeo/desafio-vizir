66b192b2198a38cd9c4e3db18c5dc3b033521af5
Léo Carvalho <carvalho.csleo@gmail.com>
2019-07-06 19:20:23 -0300

    Troca a ordem das opções

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,11 +8,11 @@
       <li class="nav-item @if($current=='home') active @endif ">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item @if($current=='falemais') active @endif ">
-        <a class="nav-link" href="/falemais">Planos Fale Mais</a>
-      </li>
       <li class="nav-item @if($current=='charge') active @endif ">
         <a class="nav-link" href="/charge">Tabela de tarifas</a>
+      </li>
+      <li class="nav-item @if($current=='falemais') active @endif ">
+        <a class="nav-link" href="/falemais">Planos Fale Mais</a>
       </li>
       <li class="nav-item @if($current=='about') active @endif ">
         <a class="nav-link" href="/about">Sobre a Telzir</a>
