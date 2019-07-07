c4f55679a47daa7f8da216d03b38f17caf3ab355
Léo Carvalho <carvalho.csleo@gmail.com>
2019-07-07 02:40:40 -0300

    Adiciona ícones à navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark rounded">
+  <a class="navbar-brand" href="/">
+    <i class="fas fa-phone"></i>
+      Telzir
+    </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -6,16 +10,29 @@
   <div class="collapse navbar-collapse" id="navbar">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item @if($current=='home') active @endif ">
-        <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('home') }}">
+          <i class="fa fa-home"></i>
+          Home
+          <span class="sr-only">(current)</span>
+        </a>
       </li>
       <li class="nav-item @if($current=='charge') active @endif ">
-        <a class="nav-link" href="{{ route('tarifas') }}">Tabela de tarifas</a>
+        <a class="nav-link" href="{{ route('tarifas') }}">
+          <i class="fas fa-dollar-sign"></i>
+          Tabela de tarifas
+        </a>
       </li>
       <li class="nav-item @if($current=='falemais') active @endif ">
-        <a class="nav-link" href="{{ route('falemais') }}">Planos Fale Mais</a>
+        <a class="nav-link" href="{{ route('falemais') }}">
+        <i class="fas fa-file-invoice-dollar"></i>
+          Planos Fale Mais
+        </a>
       </li>
       <li class="nav-item @if($current=='about') active @endif ">
-        <a class="nav-link" href="{{ route('about') }}">Sobre a Telzir</a>
+        <a class="nav-link" href="{{ route('about') }}">
+          <i class="fas fa-passport"></i>
+          Sobre a Telzir
+        </a>
       </li>
   </div>
 </nav>
