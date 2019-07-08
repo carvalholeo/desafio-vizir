7fbf191d6b416f41cf8e669cf93efad4367d814d
Léo Carvalho <carvalho.csleo@gmail.com>
2019-07-07 22:12:23 -0300

    Corrige ícones da navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,9 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/navbar.css') }}">
+
 <nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark rounded">
   <a class="navbar-brand" href="/">
-    <i class="fas fa-phone"></i>
+    <i class="fa fa-phone"></i>
       Telzir
     </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,19 +21,19 @@
       </li>
       <li class="nav-item @if($current=='charge') active @endif ">
         <a class="nav-link" href="{{ route('tarifas') }}">
-          <i class="fas fa-dollar-sign"></i>
+          <i class="fa fa-dollar-sign"></i>
           Tabela de tarifas
         </a>
       </li>
       <li class="nav-item @if($current=='falemais') active @endif ">
         <a class="nav-link" href="{{ route('falemais') }}">
-        <i class="fas fa-file-invoice-dollar"></i>
-          Planos Fale Mais
+        <i class="fa fa-file-invoice-dollar"></i>
+          Planos FaleMais
         </a>
       </li>
       <li class="nav-item @if($current=='about') active @endif ">
         <a class="nav-link" href="{{ route('about') }}">
-          <i class="fas fa-passport"></i>
+          <i class="fa fa-passport"></i>
           Sobre a Telzir
         </a>
       </li>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,23 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
     <title>
         @yield('title')
     </title>
-
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-
-    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}" />
     <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/app.css') }}" />
 
     <style>
-    body {
-        padding: 20px;
-    }
-    .navbar {
-        margin-bottom: 20px;
-    }
+        body {
+            padding: 20px;
+        }
+        .navbar {
+            margin-bottom: 20px;
+        }
     </style>
 
 </head>
